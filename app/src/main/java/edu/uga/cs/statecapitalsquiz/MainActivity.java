e9b8f4f780abab40922f12b4cce7fc2dd6fa71f1
MDCodesz <michaeladano.cs@gmail.com>
2023-11-04 18:32:33 -0400

    Testing base files

---
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.uga.cs.statecapitalsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
