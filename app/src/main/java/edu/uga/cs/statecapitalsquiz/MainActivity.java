4e19cb44608c42b10c709c6deae7d0c7076978ee
Alona0420 <you@example.com>
2023-11-06 19:47:43 -0500

    Layout and nav bar

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
@@ -1,17 +1,31 @@
 package edu.uga.cs.statecapitalsquiz;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.ContentValues;
+import android.content.res.Configuration;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = "MainActivity";
     private QuizDBHelper dbHelper; // Declare a reference to the QuizDBHelper instance
     private QuestionsData questionsData;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +37,91 @@ public class MainActivity extends AppCompatActivity {
         questionsData.open(); // Open the database
 
         CSVDataImporter.importDataFromCSV(this, questionsData);
+
+
+        // assigning ID of the toolbar to a variable
+        toolbar = findViewById( R.id.toolbar );
+        // using toolbar as ActionBar
+        setSupportActionBar( toolbar );
+        // Find our drawer view
+        drawerLayout = (DrawerLayout) findViewById( R.id.drawer_layout );
+        drawerToggle = setupDrawerToggle();
+
+        drawerToggle.setDrawerIndicatorEnabled( true );
+        drawerToggle.syncState();
+
+        // Connect DrawerLayout events to the ActionBarToggle
+        drawerLayout.addDrawerListener( drawerToggle );
+
+        // Find the drawer view
+        navigationView = findViewById( R.id.nvView );
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    selectDrawerItem( menuItem );
+                    return true;
+                });
     }
 
+    public void selectDrawerItem( MenuItem menuItem ) {
+        Fragment fragment = null;
+
+        // Create a new fragment based on the used selection in the nav drawer
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.menu_quiz) {
+            fragment = new QuizFragment();
+        } else if (itemId == R.id.menu_review) {
+            fragment = new ReviewQuizFragment();
+            //            case R.id.menu_help:
+//                fragment = new HelpFragment();
+//                break;
+//            case R.id.menu_close:
+//                finish();
+//                break;
+        } else {
+            return;
+        }
+
+        // Set up the fragment by replacing any existing fragment in the main activity
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace( R.id.fragmentContainerView, fragment).addToBackStack("main screen" ).commit();
+
+        /*
+        // this is included here as a possible future modification
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked( true );
+        // Set action bar title
+        setTitle( menuItem.getTitle());
+         */
+
+        // Close the navigation drawer
+        drawerLayout.closeDrawers();
+    }
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open,  R.string.nav_close);
+    }
+
+    @Override
+    protected void onPostCreate( Bundle savedInstanceState ) {
+        super.onPostCreate( savedInstanceState );
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged( @NonNull Configuration newConfig ) {
+        super.onConfigurationChanged( newConfig );
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged( newConfig );
+    }
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        if( drawerToggle.onOptionsItemSelected( item ) ) {
+            return true;
+        }
+        return super.onOptionsItemSelected( item );
+    }
 
 
 }
---
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/MainScreen.java
@@ -0,0 +1,71 @@
+package edu.uga.cs.statecapitalsquiz;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MainScreen#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainScreen extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MainScreen() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     *
+     * @return A new instance of fragment MainScreen.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MainScreen newInstance() {
+        MainScreen fragment = new MainScreen();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
+        super.onViewCreated( view, savedInstanceState );
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle( getResources().getString( R.string.app_name ) );
+    }
+}
