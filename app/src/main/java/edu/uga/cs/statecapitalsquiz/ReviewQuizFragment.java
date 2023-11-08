2eeb97ff0a04218a7f817fc16476ec0c1d00c085
MDCodesz <michaeladano.cs@gmail.com>
2023-11-08 01:45:12 -0500

    ReviewQuizFragment

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/ReviewQuizFragment.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/ReviewQuizFragment.java
@@ -3,11 +3,20 @@ package edu.uga.cs.statecapitalsquiz;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +25,13 @@ import android.view.ViewGroup;
  */
 public class ReviewQuizFragment extends Fragment {
 
+    private static final String TAG = "ReviewQuizFragment";
+
+    private QuizData quizData = null;
+    private List<Quiz> quizList;
+
+    private RecyclerView recyclerView;
+    private QuizRecyclerAdapter recyclerAdapter;
 
 
     public ReviewQuizFragment() {
@@ -55,5 +71,88 @@ public class ReviewQuizFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
         super.onViewCreated( view, savedInstanceState );
 
+        recyclerView = getView().findViewById( R.id.recyclerView );
+
+        // use a linear layout manager for the recycler view
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( getActivity() );
+        recyclerView.setLayoutManager( layoutManager );
+
+        quizList = new ArrayList<Quiz>();
+
+        // Create a QuizData instance, since we will need to save a new Quiz to the dn.
+        // Note that even though more activites may create their own instances of the QuizData
+        // class, we will be using a single instance of the JobLeadsDBHelper object, since
+        // that class is a singleton class.
+        quizData = new QuizData( getActivity() );
+
+        // Open that database for reading of the full list of job leads.
+        // Note that onResume() hasn't been called yet, so the db open in it
+        // was not called yet!
+        quizData.open();
+
+        // Execute the retrieval of the job leads in an asynchronous way,
+        // without blocking the main UI thread.
+        new QuizDBReader().execute();
     }
+
+    // This is an AsyncTask class (it extends AsyncTask) to perform DB reading of job leads, asynchronously.
+    private class QuizDBReader extends AsyncTask<Void, List<Quiz>> {
+        // This method will run as a background process to read from db.
+        // It returns a list of retrieved Quiz objects.
+        // It will be automatically invoked by Android, when we call the execute method
+        // in the onCreate callback (the job leads review activity is started).
+        @Override
+        protected List<Quiz> doInBackground( Void... params ) {
+            List<Quiz> quizList = quizData.retrieveAllQuizzes();
+
+            Log.d( TAG, "QuizDBReader: Job leads retrieved: " + quizList.size() );
+
+            return quizList;
+        }
+
+        // This method will be automatically called by Android once the db reading
+        // background process is finished.  It will then create and set an adapter to provide
+        // values for the RecyclerView.
+        // onPostExecute is like the notify method in an asynchronous method call discussed in class.
+        @Override
+        protected void onPostExecute( List<Quiz> qzList ) {
+            Log.d( TAG, "QuizDBReader: qzList.size(): " + qzList.size() );
+            quizList.addAll( qzList );
+
+            // create the RecyclerAdapter and set it for the RecyclerView
+            recyclerAdapter = new QuizRecyclerAdapter( getActivity(), quizList );
+            recyclerView.setAdapter( recyclerAdapter );
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Open the database
+        if( quizData != null && !quizData.isDBOpen() ) {
+            quizData.open();
+            Log.d( TAG, "ReviewQuizFragment.onResume(): opening DB" );
+        }
+
+        // Update the app name in the Action Bar to be the same as the app's name
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle( getResources().getString( R.string.app_name ) );
+    }
+
+    // We need to save job leads into a file as the activity stops being a foreground activity
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // close the database in onPause
+        if( quizData != null ) {
+            quizData.close();
+            Log.d( TAG, "ReviewQuizFragment.onPause(): closing DB" );
+        }
+    }
+
+
+
+
 }
